7890318b9f71b60140ebf849849654a9ef8f6524
cankoeks <can.koeksen@rub.de>
2025-05-02 15:37:33 +0200

    Remove whitespaces

--- src/app/examples/actions.ts
+++ src/app/examples/actions.ts
@@ -8,7 +8,6 @@ export async function getExamples() {
   return await queryExamples();
 }
 
-// In your server action file or function
 export async function create(prevState: { error: string | null, values?: { name: string, description: string } }, 
   formData: FormData) {
 
@@ -16,8 +15,6 @@ export async function create(prevState: { error: string | null, values?: { name:
   
   const name = formData.get("name") as string;
   const description = formData.get("description") as string;
-
-  // Always include the submitted values in the response
   const values = { name, description };
 
   const validationResult = validateExample({ name, description });
@@ -32,17 +29,13 @@ export async function create(prevState: { error: string | null, values?: { name:
       return { error: "An example with this name already exists", values };
     }
 
-
-
     await createExample({
       name,
       description
     });
 
-    // Revalidate the path to show the new data
     revalidatePath("/examples");
     
-    // On success, return empty values to reset the form
     return { error: null, values: { name: "", description: "" } };
   } catch (error: any) {
     return { error: error.message || "Failed to create example", values };
