24392efeb39bc7a408ab230c83e063b488f87908
Buhaiev <artemwinnerst@gmail.com>
2025-06-24 15:33:47 +0200

    clarificartion for mock

--- __mocks__/react-syntax-highlighter.js
+++ __mocks__/react-syntax-highlighter.js
@@ -1,3 +1,7 @@
+// This is not real mock.
+// It is used to avoid errors when importing react-syntax-highlighter in tests.
+// It does not provide any functionality. 
+
 module.exports = {
     Light: ({ children }) => children,
 }
